91569b59d87290c96be762324f5d73a5613f1786
d3lon-dev <delonansah87@gmail.com>
2024-09-04 19:38:25 +0000

    Parent styling done

--- src/Components/Parent/Parent.jsx
+++ src/Components/Parent/Parent.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
-import './Parent.jsx'
+import './Parent.css'
+import family from '../../assets/family.jpg'
 
 const Parent = () => {
   return (
     <div className='parent-panel'>
-      
+      <div className='parent-container'>
+        <div className="parent-row  parent-component">
+          <div className="parent-column ">
+            <h2>PARENTS AND FAMILY WEEKEND</h2>
+            <h3>October 19 - October 22, 2024</h3>
+            <h2>&nbsp;</h2>
+            <h4>The annual Parents and Family Weekend returns this fall, and provides families with an apportunity to visit their students, create life-long memories and participate in a wide variety of activities on campus.</h4>
+            <h4>&nbsp;</h4>
+            <h4>
+              <button  className='parent-button' type='button'>
+                GET REGISTRATION AND TICKETING INFO &gt;
+              </button>
+            </h4>
+          </div>
+          <div className="parent-column">
+            <img src={family} alt="" width='100%' />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
