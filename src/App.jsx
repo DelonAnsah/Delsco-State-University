1d7374537e3e6841c586041f3bfcd504c2701457
d3lon-dev <delonansah87@gmail.com>
2024-09-11 09:43:22 +0000

    Header completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import Header from './Components/Header/Header'
-import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import OpenHouse from './Components/Open House/OpenHouse'
 import SchoolRank from './Components/School Rank/SchoolRank'
@@ -10,13 +8,14 @@ import News from './Components/News/News'
 import Event from './Components/Event/Event'
 import Equity from './Components/Equity/Equity'
 import Footer from './Components/Footer/Footer'
+import Header from './Components/Header/Header'
 
 const App = () => {
   return (
-    <div>
-
+    <div className='main-container'>
+      <div className='main-header'>
       <Header/>
-      <Navbar/>
+      </div>
       <Hero/>
       <OpenHouse/>
       <SchoolRank/>
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,44 +1,61 @@
 import React from 'react'
 import './Header.css'
-import logo from '../../assets/DELSO 2.png'
+import headerlogo from '../../assets/Headerlogo.png'
 import visit_icon from '../../assets/icon-header-visit.png'
 import apply_icon from '../../assets/icon-header-apply.png'
 import give_icon from '../../assets/icon-header-give.png'
 import search_icon from '../../assets/icon-header-search.png'
-import collapse_down from '../../assets/Collapse-down.png'
-
+import glyhicon_dropdown from '../../assets/Collapse-down.png'
 
 const Header = () => {
   return (
-    <div className='header'>
-      <div className='header-container'>
-       <div className='logo'>
-        <a target='_self' href=""><img src={logo} alt="" /></a> 
+    <div className='header-panel'>
+      <div className="header-container">
+        <div className="dsulogo">
+          <div className="img-responsive">
+            <a target='self' href="#">
+              <img className='headerlogo' src={headerlogo} alt="Delsco State University" title='Delsco State University'/>
+            </a>
+          </div>
         </div>
-        <div className="Right-items">
-        <div className='header-icons'>
-        <ul>
-          <li><a href="#"><img src={visit_icon} alt="visit icon" /></a></li>
-          <li><a href="#"><img src={apply_icon} alt="apply icon" /></a></li>
-          <li><a href="#"><img src={give_icon} alt="give icon" /></a></li>
-          <li><a href="#"><img src={search_icon}alt="search icon" /></a></li>
-        </ul>
-      </div>
-      <div className='dropdown-menu'>
-        <button className='dropbtn'>Quick Links  <span><img className='arrow-down' src={collapse_down}alt="" /></span></button>
-        <div className='dropdown-content'>
-          <ul>
-            <li><a href="#">MyDSU</a></li>
-            <li><a href="#">Maps</a></li>
-            <li><a href="#">People Directory</a></li>
-            <li><a href="#">Email</a></li>
-            <li><a href="#">Careers</a></li>
+        <div className="header-icons">
+          <ul className='icon-list'>
+            <li className='header-icon' title='search'>
+              <a target='self' href="#">
+               <img title='search' src={search_icon} alt="search" />
+              </a>
+            </li>
+            <li className='header-icon' title='Give'>
+              <a target='self' href="#">
+                <img title='give' src={give_icon} alt="give" />
+              </a>
+            </li>
+            <li className='header-icon'>
+              <a target='self' href="#" title='Apply'>
+                <img title='apply' src={apply_icon} alt="apply" />
+              </a>
+            </li>
+            <li className='header-icon'>
+              <a target='self' href="#" title='visit'>
+               <img title='visit' src={visit_icon} alt="visit" />
+              </a>
+            </li>
           </ul>
-        </div>
+          <div id='quickLinksDropdown' className="dropdown dropdown-menu-right">
+            <button className='dropdown-toggle quick-links' type='button'>
+              <label> Quick Links </label>
+              <span><i class="fa-solid fa-circle-chevron-down fa-bounce"></i></span>
+            </button>
+            <ul className='dropdown-menu'>
+            <li><a href="#" title='myDSU' target='_self' >MyDSU</a></li>
+            <li><a href="#" title='Maps' target='_self' >Maps</a></li>
+            <li><a href="#" title='people Directory' target='_self' >People Directory</a></li>
+            <li><a href="#" title='Email' target='_self' >Email</a></li>
+            <li><a href="#" title='Careers' target='_self' >Careers</a></li>
+            </ul>
+          </div>
         </div>
       </div>
-      </div>
-
     </div>
   )
 }
